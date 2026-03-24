de17ad62bd7659c044b96715b1ebd5800d5406c3
Lonie <lonie6229@gmail.com>
2026-03-24 15:19:36 +0800

    First file establish

---
+++ src/main/java/com/example/LonieBot/LonieBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.LonieBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonieBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonieBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LonieBot/LonieBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LonieBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonieBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
